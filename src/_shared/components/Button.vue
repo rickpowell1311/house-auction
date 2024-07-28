7c303ec959cc37565177826ede722af08dc4d77e
Rick Powell <rick@clearly.earth>
2024-07-28 08:34:57 +0100

    Ease button hover animation

--- src/_shared/components/Button.vue
+++ src/_shared/components/Button.vue
@@ -1,7 +1,7 @@
 <script setup="lang" lang="ts">
 </script>
 <template>
-  <button class="p-2 bg-primary rounded-lg font-bold hover:scale-125 transition-all">
+  <button class="p-2 bg-primary rounded-lg font-bold hover:scale-125 transition-all duration-300">
     <slot />
   </button>
 </template>
