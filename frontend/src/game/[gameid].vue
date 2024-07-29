453f60416f83e7a84d310256befdad04378044a5
Rick Powell <rick@clearly.earth>
2024-07-29 23:57:36 +0100

    Deal 4 cards at random (example)

--- frontend/src/game/[gameid].vue
+++ frontend/src/game/[gameid].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import Button from "../_shared/components/Button.vue";
 import Main from "../_shared/components/layout/Main.vue";
 import Section from "../_shared/components/layout/Section.vue";
@@ -9,11 +9,29 @@ import Deck from "./_shared/components/Deck.vue";
 import CardContents from "./_shared/components/storyblok/CardContents.vue";
 import Table from "./_shared/components/Table.vue";
 import { useStoryblokCardDealer } from "./_shared/composables/useStoryblokCardDealer";
+import { shuffle } from "./_shared/helpers/shuffle";
 
-const { dealtCards, deal } = useStoryblokCardDealer();
+const { allCards, dealtCards, deal } = useStoryblokCardDealer();
+const usedCards = ref<number[]>([]);
+const cardsPerRound = 4;
+
+const availableCards = computed(() => {
+  return allCards.value.filter(x => !usedCards.value.includes(x));
+});
+
+const dealNext = () => {
+  const randomized = shuffle(availableCards.value);
+
+  const next = randomized
+    .slice(0, cardsPerRound <= randomized.length ? cardsPerRound : randomized.length);
+
+  usedCards.value = [...usedCards.value, ...next];
+  console.log(next);
+  deal(next);
+};
 
 onMounted(() => {
-  deal([1, 30]);
+  dealNext();
 });
 
 </script>
@@ -22,7 +40,7 @@ onMounted(() => {
   <div class="container mx-auto">
     <Main>
       <div class="flex items-center">
-        <Table>
+        <Table v-if="allCards.length > 0">
           <div class="flex gap-2 flex-wrap items-center w-full">
             <Deck />
             <template v-for="card in dealtCards" :key="card.content.value">
@@ -34,7 +52,9 @@ onMounted(() => {
             </template>
           </div>
           <Section>
-            <Button @click="() => deal([30, 1])">Redeal</Button>
+            <div v-if="availableCards.length > 0">
+              <Button @click="() => dealNext()">Deal</Button>
+            </div>
           </Section>
         </Table>
       </div>
--- frontend/src/game/_shared/composables/useStoryblokCardDealer.ts
+++ frontend/src/game/_shared/composables/useStoryblokCardDealer.ts
@@ -8,26 +8,29 @@ export const useStoryblokCardDealer = () => {
 
   const storyblokApi = useStoryblokApi();
   const allCards = ref<ISbStoryData<CardStoryblok>[]>([]);
+  const allCardNumbers = computed(() => allCards.value.map(x => Number(x.content.value)));
   const { dealt, deal } = useDealer();
-
-  const fetchCards = async () => {
-    const response = await storyblokApi.getStories({
-      content_type: "card",
-      version: import.meta.env.VITE_CMS_SHOW_DRAFTS === "true" ? "draft" : "published"
-    });
-    return response.data.stories as ISbStoryData<CardStoryblok>[];
-  };
 
   const dealtCards = computed(() => {
     return dealt.value
-      .filter(x => x.dealt)
+      .filter(x => x.dealt === true)
       .sort((a, b) => a.order - b.order)
       .map(x => allCards.value.find(y => Number(y.content.value) === x.number))
       .filter(x => x);
   });
 
+  const fetchCards = async () => {
+    const response = await storyblokApi.getStories({
+      content_type: "card",
+      version: import.meta.env.VITE_CMS_SHOW_DRAFTS === "true" ? "draft" : "published",
+      per_page: 30
+    });
+    console.log(response.data.stories.map(x => x.content?.value));
+    return response.data.stories as ISbStoryData<CardStoryblok>[];
+  };
+
   fetchCards()
     .then(result => allCards.value = result);
 
-  return { dealtCards, deal };
+  return { allCards: allCardNumbers, dealtCards, deal };
 }
