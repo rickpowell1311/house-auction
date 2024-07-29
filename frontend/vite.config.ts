4cade707a80666031ddf69df86f573f3c7a37e43
Rick Powell <rick@clearly.earth>
2024-07-29 15:05:11 +0100

    Set build target to ESNext

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [
     VueRouter({
       routesFolder: [
