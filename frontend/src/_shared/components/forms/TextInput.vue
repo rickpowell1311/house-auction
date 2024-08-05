67cd76ee892290d29ee414507faf452fd40625ee
Rick Powell <rick@clearly.earth>
2024-08-05 16:36:00 +0100

    Use ids in forms with input and label

--- frontend/src/_shared/components/forms/TextInput.vue
+++ frontend/src/_shared/components/forms/TextInput.vue
@@ -16,8 +16,8 @@ const { value, errorMessage, meta } = useField(() => props.name);
 </script>
 <template>
   <div class="relative">
-    <label :for="props.name" class="sr-only">{{ props.label }}</label>
-    <input v-model="value" :type="props.type" :name="props.name" :placeholder="placeholder"
+    <label :id="name" :for="props.name" class="sr-only">{{ props.label }}</label>
+    <input :id="name" v-model="value" :type="props.type" :name="props.name" :placeholder="placeholder"
       class="w-full rounded-md border-gray-200 pe-10 shadow-sm sm:text-sm text-white bg-secondary placeholder-gray-400">
     <ValidationError v-if="meta.dirty">{{ errorMessage }}</ValidationError>
   </div>
