01752d522506e74ee94b268124ff1bc1303d83a7
Rick Powell <rick@clearly.earth>
2024-08-07 22:54:05 +0100

    Fix button style

--- frontend/src/_shared/components/Button.vue
+++ frontend/src/_shared/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="p-2 bg-primary rounded-lg font-bold hover:scale-125 transition-all duration-300 disabled:bg-gray-400 disabled:scale-100">
+    class="p-2 bg-primary rounded-lg font-bold hover:scale-125 transition-all duration-300 disabled:bg-gray-400 disabled:scale-100 min-w-32">
     <slot />
   </button>
 </template>
