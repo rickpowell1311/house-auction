5d1d276dd71df0d27308ac0199723afee94e773a
Rick Powell <rick@clearly.earth>
2024-08-15 00:28:39 +0100

    WIP Hook up bidding phase with receiver

--- frontend/src/game/_shared/components/bidding/BiddingPhase.vue
+++ frontend/src/game/_shared/components/bidding/BiddingPhase.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { type GetBiddingPhaseResponse } from '@/_shared/providers/generated/HouseAuction.Bidding.Requests';
-import { inject, onMounted, ref, watch } from 'vue';
-import { useStoryblokCardDealer } from '../../composables/useStoryblokCardDealer';
-import { shift } from '../../helpers/shift';
-import BiddingPlayer from './BiddingPlayer.vue';
-import BiddingDeal from './BiddingDeal.vue';
-import { type SignalRClient, Key as SignalRClientKey } from '@/_shared/providers/signalRClient';
 import type { IHouseAuctionReceiver } from '@/_shared/providers/generated/TypedSignalR.Client/HouseAuction';
+import { type SignalRClient, Key as SignalRClientKey } from '@/_shared/providers/signalRClient';
+import { computed, inject, onMounted, ref } from 'vue';
+import { shift } from '../../helpers/shift';
+import BiddingDeal from './BiddingDeal.vue';
+import BiddingPlayer from './BiddingPlayer.vue';
 
 interface BiddingPhaseProps extends GetBiddingPhaseResponse {
   gameId: string;
@@ -19,11 +18,22 @@ const all = [...(props.players?.others ?? []), me];
 const others = shift(all, x => x?.order ?? 0, me)
   .filter(x => x?.name !== me?.name);
 const allBids = [props.players?.me?.bid?.amount ?? 0, ...others.map(x => x?.bid?.amount ?? 0)].sort((a, b) => b - a);
-const minBid = Math.max(...allBids) + 1;
 
 const previousPlays = ref<{ name: string, bid?: number, passed: boolean }[]>([]);
-
 const activePlayer = ref(all.find(x => x?.isTurn === true)?.name ?? undefined);
+const highestBid = ref<number>(Math.max(...allBids))
+
+const otherPlayerBidStates = computed(() => {
+  let states = {} as Record<string, number | undefined>;
+  for (const player of all) {
+    const isPass = previousPlays.value.filter(x => x.name === player?.name).some(x => x.passed);
+    states[player?.name ?? ''] = !isPass
+      ? Math.max(...previousPlays.value.filter(x => x.name === player?.name).map(x => x.bid ?? 0))
+      : undefined;
+  }
+
+  return states;
+})
 
 onMounted(() => {
   signalRClient?.subscribe({
@@ -34,6 +44,10 @@ onMounted(() => {
         passed: reaction.result?.passed === true
       }];
       activePlayer.value = reaction.nextPlayer;
+
+      if (reaction.result?.bid) {
+        highestBid.value = reaction.result.bid;
+      }
     }
   } as IHouseAuctionReceiver)
 })
@@ -44,9 +58,10 @@ onMounted(() => {
     <BiddingDeal :properties="props.deck?.propertiesOnTheTable ?? []" />
     <div class="flex gap-8 flex-wrap justify-center items-end w-full">
       <BiddingPlayer :game-id="gameId" :name="me?.name ?? ''" :is-me="true" :is-bidding="me?.name === activePlayer"
-        :coins="{ available: me?.coins ?? 0, minimum: minBid }" />
-      <BiddingPlayer v-for="player in others" :key="player?.name ?? ''" :game-id="gameId" :name="player?.name ?? ''" :is-me="false"
-        :is-bidding="player?.name === activePlayer" :coins="{ available: 0, minimum: minBid, amount: player?.bid?.amount }" />
+        :coins="{ available: me?.coins ?? 0, minimum: highestBid + 1 }" />
+      <BiddingPlayer v-for="player in others" :key="player?.name ?? ''" :game-id="gameId" :name="player?.name ?? ''"
+        :is-me="false" :is-bidding="player?.name === activePlayer"
+        :coins="{ available: 0, minimum: highestBid + 1, amount: otherPlayerBidStates[player?.name ?? ''] ?? 0 }" />
     </div>
   </div>
 </template>
