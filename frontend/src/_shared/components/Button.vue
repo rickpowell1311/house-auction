9f7dd9d6469ab0975b21846c070341652737e8df
Rick Powell <rick@clearly.earth>
2024-09-02 17:52:55 +0100

    Fix button sizes

--- frontend/src/_shared/components/Button.vue
+++ frontend/src/_shared/components/Button.vue
@@ -6,9 +6,9 @@ const props = defineProps<{
 
 <template>
   <button
-    class="p-2 rounded-lg font-bold hover:scale-125 transition-all duration-300 disabled:bg-gray-400 disabled:scale-100 min-w-32"
+    class="p-2 rounded-lg font-bold hover:scale-125 transition-all duration-300 disabled:bg-gray-400 disabled:scale-100 min-w-32 border-2 border-primary disabled:border-gray-400"
     :class="[
-      props.isSecondary === true ? 'border-primary border-2 text-primary' : 'bg-primary'
+      props.isSecondary === true ? 'text-primary' : 'bg-primary'
     ]">
     <slot />
   </button>
