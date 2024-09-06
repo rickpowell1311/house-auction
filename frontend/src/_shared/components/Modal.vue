227ff74e19600635d9c241ccd90cf1f905a7a86a
Rick Powell <rick@clearly.earth>
2024-09-06 15:02:03 +0100

    Show errors when backend unavailable

--- frontend/src/_shared/components/Modal.vue
+++ frontend/src/_shared/components/Modal.vue
@@ -2,7 +2,7 @@
   <div class="z-20 w-screen h-screen fixed top-0 left-0 translate-x-0">
     <div class="relative w-full h-full">
       <div class="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-70" />
-      <div class="w-full h-full flex items-center justify-center">
+      <div class="absolute w-full h-full flex items-center justify-center z-10">
         <slot />
       </div>
     </div>
--- frontend/src/_shared/providers/SignalR.vue
+++ frontend/src/_shared/providers/SignalR.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { onBeforeMount, provide, ref } from 'vue';
+import Section from '../components/layout/Section.vue';
+import Loader from '../components/Loader.vue';
+import Modal from '../components/Modal.vue';
 import { getHubProxyFactory, getReceiverRegister } from './generated/TypedSignalR.Client';
 import { Key, type SignalRClient } from './signalRClient';
 
 const connection = ref(new HubConnectionBuilder()
   .withUrl(`${import.meta.env.VITE_SIGNALR_URL}/house-auction`, { withCredentials: false })
   .build());
+
 const connectionReady = ref(false);
+const connectionError = ref<string | undefined>();
 
 const hub = getHubProxyFactory("IHouseAuctionHub")
   .createHubProxy(connection.value as HubConnection);
@@ -24,6 +29,7 @@ onBeforeMount(async () => {
     await connection.value.start();
     connectionReady.value = true;
   } catch (error) {
+    connectionError.value = String(error);
     console.error(error);
   }
 })
@@ -31,4 +37,15 @@ onBeforeMount(async () => {
 
 <template>
   <slot v-if="connectionReady" />
+  <Loader v-else-if="!connectionError" />
+  <Modal v-else title="Connection Error">
+    <div class="container mx-auto">
+      <Section class="animate-bounce">
+        <h1 class="text-center animate-wiggle animate-infinite">😭</h1>
+      </Section>
+      <Section>
+        <h2 class="shadow-gray-300 text-primary text-center">House Auction is unavailable right now</h2>
+      </Section>
+    </div>
+  </Modal>
 </template>
