c6cfa380f777cae64703d8bb8037cc1811075aca
Rick Powell <rick@clearly.earth>
2024-09-06 15:48:33 +0100

    Add background

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import Background from './_shared/components/layout/Background.vue';
 import SignalR from './_shared/providers/SignalR.vue';
+
+const dimension = 12;
+
+const rows = Array.from(Array(dimension).keys());
+const cols = Array.from(Array(dimension).keys());
 </script>
 
 <template>
   <div class="h-screen w-screen">
+    <Background class="fixed h-screen w-screen -z-10" />
     <SignalR>
       <RouterView />
     </SignalR>
---
+++ frontend/src/_shared/components/layout/Background.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { PhHouse } from '@phosphor-icons/vue';
+
+const dimension = 12;
+
+const rows = Array.from(Array(dimension).keys());
+const cols = Array.from(Array(dimension).keys());
+</script>
+<template>
+  <div class="h-full w-full top-0 left-0 opacity-5 text-gray-600 flex flex-col justify-between gap-8 overflow-clip">
+    <div v-for="col in cols" :key="col">
+      <div class="h-full flex justify-between gap-8">
+        <PhHouse v-for="item in rows" :key="item" class="w-full h-full max-w-24 max-h-24 -rotate-45 animate-wiggle" />
+      </div>
+    </div>
+  </div>
+</template>
